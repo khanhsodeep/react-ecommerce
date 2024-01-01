3213cc494f3c956b45f94592ed9ccd1593b8ff89
quockhanhwork <quockhanh.vlu@gmail.com>
2024-01-01 14:28:06 +0700

    add assets and images p2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'swiper/css';
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// fonts and icons
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
 import {
   createBrowserRouter,
   RouterProvider,
