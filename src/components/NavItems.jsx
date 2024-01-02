13fcaaa6655f1f20f25045095a0e4d324032d7c1
quockhanhwork <quockhanh.vlu@gmail.com>
2024-01-02 14:10:18 +0700

    code navbar  v4 add menu area

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,51 +1,77 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/images/logo/logo.png'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo/logo.png";
 
 const NavItems = () => {
-    const [menuToogle, setMenutoggle] = useState(false)
-    const [socialToggle, setSocialToogle] = useState(false)
-    const [headerFixed, setHeaderFixed] = useState(false)
+  const [menuToogle, setMenutoggle] = useState(false);
+  const [socialToggle, setSocialToogle] = useState(false);
+  const [headerFixed, setHeaderFixed] = useState(false);
 
-    // Add event listener
+  // Add event listener
 
-    window.addEventListener("scroll", ()=>{
-        if(window.scrollY >200){
-            setHeaderFixed(true)
-        }
-        else{
-            setHeaderFixed(false)
-        }
-    })
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setHeaderFixed(true);
+    } else {
+      setHeaderFixed(false);
+    }
+  });
   return (
-    <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp":""}`}>
-        {/* {header top start} */}
-        <div className={`header-top d-md-none ${socialToggle ? "open":""}`}>
-            <div className='container'>
-                <div className='header-top-area'>
-                    <Link to="/signup" className='lab-btn me-3'><span>Create Account</span></Link>
-                    <Link to="/login">Login</Link>
-                </div>
-            </div>
+    <header
+      className={`header-section style-4 ${
+        headerFixed ? "header-fixed fadeInUp" : ""
+      }`}
+    >
+      {/* {header top start} */}
+      <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
+        <div className="container">
+          <div className="header-top-area">
+            <Link to="/signup" className="lab-btn me-3">
+              <span>Create Account</span>
+            </Link>
+            <Link to="/login">Login</Link>
+          </div>
         </div>
-        {/* {Header botton} */}
-        <div className='header-bottom'>
-            <div className='container'>
-                <div className='header-wrapper'>
-                    {/* {logo} */}
-                    <div className='logo-search-acte'>
-                        <div className='logo'>
-                                <Link to={"/"}>
-                                    <img src={logo} alt="" />
-                                </Link>
-                        </div>
-                    </div>
-                </div>
-
+      </div>
+      {/* {Header bottom} */}
+      <div className="header-bottom">
+        <div className="container">
+          <div className="header-wrapper">
+            {/* {logo} */}
+            <div className="logo-search-acte">
+              <div className="logo">
+                <Link to={"/"}>
+                  <img src={logo} alt="" />
+                </Link>
+              </div>
             </div>
-        </div>   
+            {/* {Menu area} */}
+            <div className="menu-area">
+              <div className="menu">
+                <ul className={`lab-ul ${menuToogle ? "active": ""}`}>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/shop">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
