b583d9498af200d8f8df1f2d16f729bedbd7f74e
quockhanhwork <quockhanh.vlu@gmail.com>
2024-01-04 15:11:07 +0700

    code banner search product

---
+++ src/home/Banner.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import productData from "../products.json";
+import { Link } from "react-router-dom";
+
+const title = (
+  <h2>
+    Search Your One From <span>Thousand</span> of Products
+  </h2>
+);
+const desc = "We have  the largest collection of products";
+const bannerList = [
+  {
+    iconName: "icofont-users-alt-4",
+    text: "1.5 Million Customers",
+  },
+  {
+    iconName: "icofont-notification",
+    text: "More then 2000 Marchent",
+  },
+  {
+    iconName: "icofont-globe",
+    text: "Buy Anything Online",
+  },
+];
+const Banner = () => {
+  const [searchInput, setSeachInput] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(productData);
+  // console.log(productData)
+
+  function handleSearch(e) {
+    const searchTerm = e.target.value;
+    setSeachInput(searchTerm);
+    //    filter products based on search
+    const filtered = productData.filter((product) =>
+      product.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+    setFilteredProducts(filtered);
+  }
+  return (
+    <div className="banner-section style-4">
+      <div className="container">
+        <div className="banner-content">
+          {title}
+          <form>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search Your Product"
+              value={searchInput}
+              onChange={handleSearch}
+            />
+          </form>
+          <p>{desc}</p>
+          <ul className="lab-ul">
+            {searchInput &&
+              filteredProducts.map((product, i) => (
+                <li key={i}>
+                  <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
