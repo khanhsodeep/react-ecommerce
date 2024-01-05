4fa6a2e2830c367760d239c46ec7b2c5e238c2c8
quockhanhwork <quockhanh.vlu@gmail.com>
2024-01-05 14:59:58 +0700

    code btn get started

--- src/home/HomeCategory.jsx
+++ src/home/HomeCategory.jsx
@@ -44,41 +44,47 @@ const categoryList = [
 ];
 
 const HomeCategory = () => {
-  return <div className="category-section style-4 padding-tb">
-    <div className="container">
+  return (
+    <div className="category-section style-4 padding-tb">
+      <div className="container">
         {/* section header */}
         <div className="section-header text-center">
-                <span className="subtitle">{subTitle}</span>
-                <h2 className="title">{title}</h2>
+          <span className="subtitle">{subTitle}</span>
+          <h2 className="title">{title}</h2>
         </div>
         {/* section card */}
         <div className="section-wrapper">
-            <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
-                {
-                   categoryList.map((val,i)=>(
-                    <div key={i} className="col">
-                        <Link to="/shop" className="category-item">
-                            <div className="category-inner">
-                                {/* image thumnail */}
-                                <div className="category-thumb">
-                                    <img src={val.imgUrl} alt={val.title} />
-                                </div>
-                                {/* content */}
-                                <div className="category-content">
-                                    <div className="cate-icon">
-                                        <i className={val.iconName}></i>
-                                    </div>
-                                    <Link to="/shop"><h6>{val.title}</h6></Link>
-                                </div>
-                            </div>
-                        </Link>
+          <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
+            {categoryList.map((val, i) => (
+              <div key={i} className="col">
+                <Link to="/shop" className="category-item">
+                  <div className="category-inner">
+                    {/* image thumnail */}
+                    <div className="category-thumb">
+                      <img src={val.imgUrl} alt={val.title} />
                     </div>
-                   ))
-                }    
-            </div> 
+                    {/* content */}
+                    <div className="category-content">
+                      <div className="cate-icon">
+                        <i className={val.iconName}></i>
+                      </div>
+                      <Link to="/shop">
+                        <h6>{val.title}</h6>
+                      </Link>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+                {/* btn get started */}
+          <div className="text-center mt-5">
+            <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
+          </div>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default HomeCategory;
