63af722a5feaf1ce78fcda1549c9b89c446bf5e2
quockhanhwork <quockhanh.vlu@gmail.com>
2024-01-05 15:36:04 +0700

    code categorShowCase/section header

--- src/home/CategoryShowCase.jsx
+++ src/home/CategoryShowCase.jsx
@@ -78,15 +78,42 @@ const ProductData = [
 ];
 
 const CategoryShowCase = () => {
-    const [items, setItems] = useState(ProductData);
-  return <div className="course-section style-3 padding-tb">
-        <div className="course-shape one">
-            <img src="/src/assets/images/shape-img/icon/01.png" alt="" />
-        </div>
-        <div className="course-shape two">
+  const [items, setItems] = useState(ProductData);
+  const filterItem = () =>{
+
+  }
+  return (
+    <div className="course-section style-3 padding-tb">
+        {/* shapes */}
+      <div className="course-shape one">
+        <img src="/src/assets/images/shape-img/icon/01.png" alt="" />
+      </div>
+      <div className="course-shape two">
         <img src="/src/assets/images/shape-img/icon/02.png" alt="" />
+      </div>
+      {/* main section */}
+      <div className="container">
+        {/* section header */}
+        <div className="section-header">
+            <h2 className="title">{title}</h2>
+
+            <div className="course-filter-group">
+                <ul className="lab-ul">
+                    <li onClick={()=>filterItem("All")}>All</li>
+                    <li onClick={()=>filterItem("Shoes")}>Shoes</li>
+
+                    <li onClick={()=>filterItem("Bags")}>Bags</li>
+
+                    <li onClick={()=>filterItem("Phones")}>Phones</li>
+
+                    <li onClick={()=>filterItem("Beauty")}>Beauty</li>
+
+                </ul>
+            </div>
         </div>
-  </div>;
+      </div>
+    </div>
+  );
 };
 
 export default CategoryShowCase;
