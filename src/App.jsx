7f0bec63577c5a243cbb8f4f4ecd99b539db74e5
quockhanhwork <quockhanh.vlu@gmail.com>
2024-02-25 14:00:39 +0700

    code Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {Outlet} from "react-router-dom"
 import './App.css'
 import NavItems from "./components/NavItems"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <>
       <NavItems/>
-      <Outlet/>   
+      <Outlet/> 
+      <Footer/>  
     </>
   )
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
       <AboutUs/>
       <AppSection/>
       <Sponsor/>
+     
     </div>
   );
 };
